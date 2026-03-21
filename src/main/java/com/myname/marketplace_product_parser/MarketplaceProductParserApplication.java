cd7aca272448b90b627952633dfc5f70055d6ac8
Yurii Midianyi <loopfordev@gmail.com>
2026-03-22 00:42:40 +0100

    Initial commit: Marketplace Product Parser structure

---
+++ src/main/java/com/myname/marketplace_product_parser/MarketplaceProductParserApplication.java
@@ -0,0 +1,13 @@
+package com.myname.marketplace_product_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceProductParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceProductParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myname/marketplace_product_parser/MarketplaceProductParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myname.marketplace_product_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceProductParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
