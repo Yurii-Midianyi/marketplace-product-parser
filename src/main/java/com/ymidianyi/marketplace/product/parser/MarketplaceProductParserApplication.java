01434d60c0acd25396b8642d8ddc1e5cfc9973d3
Yurii Midianyi <loopfordev@gmail.com>
2026-04-01 14:56:44 +0200

    add FileProcessingProperties

--- src/main/java/com/ymidianyi/marketplace/product/parser/MarketplaceProductParserApplication.java
+++ src/main/java/com/ymidianyi/marketplace/product/parser/MarketplaceProductParserApplication.java
@@ -2,12 +2,13 @@ package com.ymidianyi.marketplace.product.parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MarketplaceProductParserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketplaceProductParserApplication.class, args);
 	}
-
 }
