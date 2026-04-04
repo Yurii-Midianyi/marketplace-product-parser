f400164fe72f152832e38f7f1049d0b36126a8e8
Yurii Midianyi <loopfordev@gmail.com>
2026-04-04 21:12:57 +0200

    use proper logging instead of sout

--- src/main/java/com/ymidianyi/marketplace/product/parser/config/FileProcessingProperties.java
+++ src/main/java/com/ymidianyi/marketplace/product/parser/config/FileProcessingProperties.java
@@ -1,5 +1,7 @@
 package com.ymidianyi.marketplace.product.parser.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.io.IOException;
@@ -8,7 +10,9 @@ import java.nio.file.Path;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @ConfigurationProperties(prefix = "file-processing")
 public class FileProcessingProperties {
 
@@ -46,11 +50,11 @@ public class FileProcessingProperties {
         catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println(inputDir);
-        System.out.println(processedDir);
-        System.out.println(failedDir);
-        System.out.println(cron);
-        System.out.println(threadPoolSize);
-        System.out.println(maxExportAgeDays);
+        log.info(inputDir);
+        log.info(processedDir);
+        log.info(failedDir);
+        log.info(cron);
+        log.info(String.valueOf(threadPoolSize));
+        log.info(String.valueOf(maxExportAgeDays));
     }
 }
