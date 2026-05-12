08952914ffaa3bb3972a6e8063a42aff5af61723
Yurii Midianyi <loopfordev@gmail.com>
2026-05-12 18:13:34 +0200

    add basic dto validation and ValidationResult

--- src/main/java/com/ymidianyi/marketplace/product/parser/dto/ProductDto.java
+++ src/main/java/com/ymidianyi/marketplace/product/parser/dto/ProductDto.java
@@ -1,19 +1,22 @@
 package com.ymidianyi.marketplace.product.parser.dto;
 
 import com.ymidianyi.marketplace.product.parser.model.ProductState;
-
+import org.hibernate.validator.constraints.URL;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-public record ProductDto(String name,
-                         String sku,
-                         BigDecimal price,
-                         BigDecimal specialPrice,
+public record ProductDto(@NotBlank String name,
+                         @NotBlank String sku,
+                         @NotNull @Positive BigDecimal price,
+                         @Positive BigDecimal specialPrice,
                          LocalDate specialFrom,
                          LocalDate specialTo,
-                         ProductState state,
+                         @NotNull ProductState state,
                          String brand,
                          List<String> categories,
-                         String imageUrl) {
+                         @URL String imageUrl) {
 }
--- src/main/java/com/ymidianyi/marketplace/product/parser/dto/ProductExportFileDto.java
+++ src/main/java/com/ymidianyi/marketplace/product/parser/dto/ProductExportFileDto.java
@@ -3,5 +3,9 @@ package com.ymidianyi.marketplace.product.parser.dto;
 import java.time.Instant;
 import java.util.List;
 
-public record ProductExportFileDto(String partnerId, Instant exportDate, List<ProductDto> products) {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+public record ProductExportFileDto(@NotBlank String partnerId, @NotBlank Instant exportDate, @NotEmpty List<@Valid ProductDto> products) {
 }
