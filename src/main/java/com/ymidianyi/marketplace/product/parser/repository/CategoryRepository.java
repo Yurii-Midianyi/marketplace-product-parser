058579358f834cd2ede9352fc8d40a5c4767e3f0
antoninaolhovik <toshaolhovik@gmail.com>
2026-05-17 21:18:40 +0300

    Add category repository

---
+++ src/main/java/com/ymidianyi/marketplace/product/parser/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.ymidianyi.marketplace.product.parser.repository;
+
+import com.ymidianyi.marketplace.product.parser.model.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+}
