7b4ad48f888e353091f0483dc8aaecaf974ac90c
Yurii Midianyi <loopfordev@gmail.com>
2026-05-20 20:09:15 +0200

    add test utilities class

---
+++ src/test/java/com/ymidianyi/marketplace/product/parser/TestUtilities.java
@@ -0,0 +1,25 @@
+package com.ymidianyi.marketplace.product.parser;
+
+import com.ymidianyi.marketplace.product.parser.dto.ProductDto;
+import com.ymidianyi.marketplace.product.parser.model.ProductState;
+
+import java.math.BigDecimal;
+
+public class TestUtilities {
+
+    public static ProductDto createCheeseProduct() {
+        return new ProductDto(
+                "Cheese",
+                "5645",
+                BigDecimal.valueOf(500),
+                null,
+                null,
+                null,
+                ProductState.ACTIVE,
+                null,
+                null,
+                null
+        );
+    }
+}
+
