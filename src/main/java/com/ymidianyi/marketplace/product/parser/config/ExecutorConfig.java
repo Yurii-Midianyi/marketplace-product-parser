7c0b0ea1dbcd330e1e9086dd13e6b421996c7aee
Yurii Midianyi <loopfordev@gmail.com>
2026-06-25 20:32:12 +0200

    Add executor config

---
+++ src/main/java/com/ymidianyi/marketplace/product/parser/config/ExecutorConfig.java
@@ -0,0 +1,38 @@
+package com.ymidianyi.marketplace.product.parser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class ExecutorConfig {
+    private ExecutorService executorService;
+
+    @Bean
+    public ExecutorService executorService(FileProcessingProperties properties) {
+        this.executorService = Executors.newFixedThreadPool(properties.getThreadPoolSize());
+        log.info("Executor service started with pool size of {}", properties.getThreadPoolSize());
+        return executorService;
+    }
+
+    @PreDestroy
+    public void shutdown() throws InterruptedException {
+        if(executorService == null) {
+            return;
+        }
+        log.info("Executor service shutting down");
+        executorService.shutdown();
+        if(!executorService.awaitTermination(30, TimeUnit.SECONDS)){
+            log.error("Executor service did not terminate within 30 seconds, forcing shutdown");
+            executorService.shutdownNow();
+        }
+        log.info("Executor service shut down");
+    }
+}
